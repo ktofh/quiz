f13f0e96ef002512e8290d64fab4d4f16b6dd20a
ktofh <iluhaodessit@gmail.com>
2022-07-17 18:11:01 +0300

    rer

--- fet.js
+++ fet.js
@@ -1,22 +1,47 @@
-const cot = confirm("есть ли вам 18 лет?");
-if (cot == false){
-    alert('Вам запрещено проходить опрос')
-};
-const ger =prompt('Во сколько вы встаете');
-if (ger ===1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ,12, 13, 14, 15 , 16, 17 ,18 , 19, 20, 21, 22 , 23, 24){
-    true
-}else{
-    alert('некорректный ввод')
-}
-if (ger<5){
-    alert("Очень рано")
-}else if (5>ger && ger>8){
-    alert("Всё равно рано")
-}else if (8>ger && ger>11){
-    alert("Нормально")
-}else if (11>ger && ger>14){
-    alert("Слишком поздно")
-}else if (14>ger){
-   alert("Очень-очень поздно")
-}
+const quiz = confirm("есть ли вам 18 лет?");
+if (quiz){
 
+    const questionOne = prompt("Во сколько вы встаете?")
+
+    const number = Number(questionOne);
+
+    if(questionOne !== null && !isNaN(number)) {
+
+            
+            if(number <= 5) {
+
+                alert("Очень рано")
+
+            } else if (number > 5 && number <= 8) {
+                
+                alert("Всё равно рано")
+
+            } else if (number > 8 && number <= 11) {
+
+                alert("Нормально")
+
+            } else if (number > 11 && number <= 14) {
+
+                alert("Слишком поздно")
+
+            } else if (number > 14 && number <= 24) {
+
+                alert("Очень-очень поздно")
+
+            } else {
+
+                alert("Научись пользоваться часами!")
+
+            }
+        
+    } else {
+
+        alert("Введите число")
+
+    }
+
+} else {
+
+    alert('Вам запрещено проходить опрос')
+
+}
